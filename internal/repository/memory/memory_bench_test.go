7d84b380f6eeeba9e0ebe0f55897aee14c26bf84
Deryugin Nikolay Nikolaevich <nderyugin@ozon.ru>
2026-07-22 23:09:05 +0300

    [YAPRACT] Fix review

--- internal/repository/memory/memory_bench_test.go
+++ internal/repository/memory/memory_bench_test.go
@@ -1,83 +1,91 @@
 package memory_test
 
 import (
-	"context"
 	"testing"
 
-	"github.com/fireflg/go-musthave-metrics-tpl/internal/repository/memory"
 	models "github.com/fireflg/go-musthave-metrics-tpl/internal/model"
+	"github.com/fireflg/go-musthave-metrics-tpl/internal/repository/memory"
 )
 
 func BenchmarkSetGauge(b *testing.B) {
+	b.StopTimer()
 	repo := memory.NewMemoryRepository()
-	ctx := context.Background()
+	ctx := b.Context()
+	var i int64
+	b.StartTimer()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		name := "gauge_metric_" + string(rune('0'+i%10))
-		repo.SetGauge(ctx, name, float64(i))
+	for b.Loop() {
+		repo.SetGauge(ctx, "gauge_metric", float64(i))
+		i++
 	}
 }
 
 func BenchmarkSetCounter(b *testing.B) {
+	b.StopTimer()
 	repo := memory.NewMemoryRepository()
-	ctx := context.Background()
+	ctx := b.Context()
+	var i int64
+	b.StartTimer()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		name := "counter_metric_" + string(rune('0'+i%10))
-		repo.SetCounter(ctx, name, int64(i))
+	for b.Loop() {
+		repo.SetCounter(ctx, "counter_metric", i)
+		i++
 	}
 }
 
 func BenchmarkGetGauge(b *testing.B) {
+	b.StopTimer()
 	repo := memory.NewMemoryRepository()
-	ctx := context.Background()
+	ctx := b.Context()
 
-	// Pre-populate
 	for i := 0; i < 100; i++ {
 		repo.SetGauge(ctx, "gauge_metric", float64(i))
 	}
+	b.StartTimer()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		repo.GetGauge(ctx, "gauge_metric")
 	}
 }
 
 func BenchmarkGetCounter(b *testing.B) {
+	b.StopTimer()
 	repo := memory.NewMemoryRepository()
-	ctx := context.Background()
+	ctx := b.Context()
 
-	// Pre-populate
 	for i := 0; i < 100; i++ {
 		repo.SetCounter(ctx, "counter_metric", int64(i))
 	}
+	b.StartTimer()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		repo.GetCounter(ctx, "counter_metric")
 	}
 }
 
 func BenchmarkSetMetric_Gauge(b *testing.B) {
+	b.StopTimer()
 	repo := memory.NewMemoryRepository()
-	ctx := context.Background()
+	ctx := b.Context()
+	var i int64
+	b.StartTimer()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
+	for b.Loop() {
 		value := float64(i)
 		repo.SetMetric(ctx, models.Metrics{ID: "gauge_metric", MType: "gauge", Value: &value})
+		i++
 	}
 }
 
 func BenchmarkSetMetric_Counter(b *testing.B) {
+	b.StopTimer()
 	repo := memory.NewMemoryRepository()
-	ctx := context.Background()
+	ctx := b.Context()
+	var i int64
+	b.StartTimer()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		delta := int64(i)
-		repo.SetMetric(ctx, models.Metrics{ID: "counter_metric", MType: "counter", Delta: &delta})
+	for b.Loop() {
+		repo.SetMetric(ctx, models.Metrics{ID: "counter_metric", MType: "counter", Delta: &i})
+		i++
 	}
 }
